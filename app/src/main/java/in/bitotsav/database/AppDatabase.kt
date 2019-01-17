5a1651bd9f016bd5e3f69722b6f56c7f75bc7ecb
Ashank Anshuman <ashankanshuman@gmail.com>
2019-01-18 02:40:11 +0530

    Add Team entity

--- app/src/main/java/in/bitotsav/database/AppDatabase.kt
+++ app/src/main/java/in/bitotsav/database/AppDatabase.kt
@@ -2,10 +2,13 @@ package `in`.bitotsav.database
 
 import `in`.bitotsav.events.Event
 import `in`.bitotsav.events.EventDao
+import `in`.bitotsav.teams.TeamDao
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [Event::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun eventDao() : EventDao
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun eventDao(): EventDao
+
+    abstract fun teamDao(): TeamDao
 }
