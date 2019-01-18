d2f07b273b6b3331a220c3164fe5ce3062d62b01
Ashank Anshuman <ashankanshuman@gmail.com>
2019-01-18 20:23:29 +0530

    Add SingletonHolder and build DB

--- app/src/main/java/in/bitotsav/database/AppDatabase.kt
+++ app/src/main/java/in/bitotsav/database/AppDatabase.kt
@@ -3,8 +3,11 @@ package `in`.bitotsav.database
 import `in`.bitotsav.events.Event
 import `in`.bitotsav.events.EventDao
 import `in`.bitotsav.feed.FeedDao
+import `in`.bitotsav.shared.SingletonHolder
 import `in`.bitotsav.teams.TeamDao
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Event::class], version = 1)
@@ -14,4 +17,10 @@ abstract class AppDatabase: RoomDatabase() {
     abstract fun teamDao(): TeamDao
 
     abstract fun feedDao() : FeedDao
+
+    companion object : SingletonHolder<AppDatabase, Context>({
+        Room.databaseBuilder(it.applicationContext,
+            AppDatabase::class.java, "App.db")
+            .build()
+    })
 }
