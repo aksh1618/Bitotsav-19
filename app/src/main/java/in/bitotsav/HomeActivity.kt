e3af4507aee83561fef237e15cdb0fb938499da6
aksh1618 <aakarshituppal@gmail.com>
2019-01-25 04:39:30 +0530

    Improve navigation bar ↗

--- app/src/main/java/in/bitotsav/HomeActivity.kt
+++ app/src/main/java/in/bitotsav/HomeActivity.kt
@@ -2,8 +2,7 @@ package `in`.bitotsav
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -13,20 +12,21 @@ class HomeActivity : AppCompatActivity() {
         private const val TAG = "HomeActivity"
     }
 
-    private val navHostFragment: NavHostFragment by lazy {
-        supportFragmentManager.findFragmentById(R.id.mainFragment) as NavHostFragment
-    }
+//    private val navHostFragment: NavHostFragment by lazy {
+//        supportFragmentManager.findFragmentById(R.id.mainFragment) as NavHostFragment
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val navController = navHostFragment.navController
-        setupBottomNavMenu(navController)
+//        val navController = navHostFragment.navController
+        setupBottomNavMenu()
 
     }
 
-    private fun setupBottomNavMenu(navController: NavController) {
+    private fun setupBottomNavMenu() {
+        val navController = Navigation.findNavController(this, R.id.mainFragment)
         mainNavigation.setupWithNavController(navController)
     }
 }
