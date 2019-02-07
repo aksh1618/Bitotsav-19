7494cd067ccf193138e172d35eb51f0a7cc2c841
Ashank Anshuman <ashankanshuman@gmail.com>
2019-02-07 14:14:35 +0530

    Add eventName to User teams.

--- app/src/main/java/in/bitotsav/profile/data/User.kt
+++ app/src/main/java/in/bitotsav/profile/data/User.kt
@@ -1,5 +1,6 @@
 package `in`.bitotsav.profile.data
 
+import `in`.bitotsav.events.data.EventRepository
 import `in`.bitotsav.profile.CurrentUser
 import `in`.bitotsav.teams.championship.data.ChampionshipTeamRepository
 import `in`.bitotsav.teams.nonchampionship.data.NonChampionshipTeamRepository
@@ -17,7 +18,7 @@ data class User(
     val championshipTeam: String? = null
 ) : KoinComponent {
     var score: Int = runBlocking { championshipTeam?.let { get<ChampionshipTeamRepository>().getScoreByName(it) } ?: 0 }
-    // [{"eventId":{name:"eventName",rank:"rank"**}}]  Note: All values are strings
+    // [{"eventId":{eventName:"eventName",teamName:"teamName",rank:"rank"**}}]  Note: All values are strings
     var teams: Map<String, Map<String, String>> = getUserTeams()
     //    [{id:{email, name}}]
     var members: Map<String, Map<String, String>> = getTeamMembers()
@@ -25,11 +26,15 @@ data class User(
     private fun getUserTeams(): Map<String, Map<String, String>> {
         val userTeams = mutableMapOf<String, Map<String, String>>()
         val teams = runBlocking { get<NonChampionshipTeamRepository>().getAllUserTeams() }
-        teams?.forEach {
-            userTeams[it.eventId.toString()] = mapOf(
-                "name" to it.name,
-                "rank" to it.rank.toString()
-            )
+        teams?.forEach { team ->
+            val eventName = runBlocking { get<EventRepository>().getNameById(team.eventId) }
+            eventName?.let {
+                userTeams[team.eventId.toString()] = mapOf(
+                    "eventName" to eventName,
+                    "teamName" to team.name,
+                    "rank" to team.rank.toString()
+                )
+            }
         }
         return userTeams.toMap()
     }
