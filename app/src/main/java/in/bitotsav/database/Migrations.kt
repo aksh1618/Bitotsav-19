340cea7034153080bf2e217de35291492a4e38c8
Ashank Anshuman <ashankanshuman@gmail.com>
2019-02-13 23:30:31 +0530

    Fix migration from V1 to V2.

--- app/src/main/java/in/bitotsav/database/Migrations.kt
+++ app/src/main/java/in/bitotsav/database/Migrations.kt
@@ -5,11 +5,11 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE User ADD COLUMN day1 BOOLEAN")
-        database.execSQL("ALTER TABLE User ADD COLUMN day2 BOOLEAN")
-        database.execSQL("ALTER TABLE User ADD COLUMN day3 BOOLEAN")
-        database.execSQL("ALTER TABLE User ADD COLUMN merchandise BOOLEAN")
-        database.execSQL("ALTER TABLE User ADD COLUMN accommodation BOOLEAN")
+        database.execSQL("ALTER TABLE User ADD COLUMN day1 INTEGER NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE User ADD COLUMN day2 INTEGER NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE User ADD COLUMN day3 INTEGER NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE User ADD COLUMN merchandise INTEGER NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE User ADD COLUMN accommodation INTEGER NOT NULL DEFAULT 0")
     }
 
 }
